a3b8d71aaebae54f268fffe430ed2b33b2756c56
santhosh <santhoshg1990@gmail.com>
2023-09-23 00:26:13 +0530

    base code added with route

---
+++ src/route.js
@@ -0,0 +1,52 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <>Hello World!</>,
+    },
+    {
+        path: '/:customer_sid/profile',
+        element: <>Customer Profile</>
+    },
+    {
+        path: '/:customer_sid/users',
+        element: <>Users List</>
+    },
+    {
+        path: '/:customer_sid/users/new',
+        element: <>Create user</>
+    },
+    {
+        path: '/:customer_sid/users/:user_sid',
+        element: <>Update user</>
+    },
+    {
+        path: '/:customer_sid/reviews',
+        element: <>Customer reviews</>
+    },
+    {
+        path: '/:customer_sid/reviews/:user_sid/new',
+        element: <>User provides review for customer</>
+    },
+    {
+        path: '/:customer_sid/reviews/:user_sid/',
+        element: <>View User reviews</>
+    },
+    {
+        path: '/:customer_sid/review-template/new',
+        element: <>Create Review Template</>
+    },
+    {
+        path: '/:customer_sid/review-template/:review_template_sid',
+        element: <>Edit Review Template</>
+    },
+    {
+        path: '/login',
+        element: <>Customer Login</>
+    },
+    {
+        path: '/onboard',
+        element: <>Customer onboarding</>
+    }
+]);
