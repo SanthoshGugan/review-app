b3dafac52e2846875089a85358503058c51ce01c
santhosh <santhoshg1990@gmail.com>
2023-10-05 11:25:10 +0530

    Verify email trigger twice bug is fixed

--- src/component/VerifyEmail.js
+++ src/component/VerifyEmail.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { updateCustomerApi, verifyPasscodeApi as verifyEmailApi } from "../api/CustomerApi";
-import { updateCustomerPasswordRequest, verifyPasscodeRequest } from "../utils/CustomerUtils";
+import { verifyPasscodeApi as verifyEmailApi } from "../api/CustomerApi";
+import { verifyPasscodeRequest } from "../utils/CustomerUtils";
 import { NavLink, useParams, useSearchParams } from "react-router-dom";
-import { Alert, AlertIcon, Box, Flex, VStack, useToast } from "@chakra-ui/react";
+import { Alert, AlertIcon, Flex, useToast } from "@chakra-ui/react";
 import CenterCard from "../lib/CenterCard";
-import RInput from "../lib/Input";
-import { getButtonFormProps, getInputFormProps } from "../utils/formUtil";
-import RButton from "../lib/Button";
 import { CUSTOMER_LOGIN_URL } from "../utils/urlUtil";
 
 const VerifyEmail = (props) => {
@@ -18,33 +15,7 @@ const VerifyEmail = (props) => {
     const [ isVerificationSuccess, setIsVerificationSuccess ] = useState(false);
     const [ isVerificationComplete, setIsVerificationComplete ] = useState(false);
 
-    const [ isPasswordUpdatedSuccess, setIsPasswordSuccessUpdated ] = useState(false);
-    const [ isPasswordUpdateComplete, setIsPasswordUpdateComplete ] = useState(false);
-
-    const {
-        handleSubmit,
-        register,
-        formState: { errors, isSubmitting, isValid },
-        getFieldState
-    } = useForm({
-        mode: "onTouched"
-    });
-
-    const form_props = {
-        errors,
-        register,
-        getFieldState
-    };
-
-
     const toast = useToast();
-
-    const {
-        handleSubmit: handlePasswordSubmit,
-        register: passwordRegister,
-        formState: { errors: passwordErrors, isSubmitting: isSubmittingPassword, isValid: isValidPassword },
-        getFieldState: passwordGetFieldState,
-    } = useForm();
 
     const verifyToken = async () => {
         try {
@@ -88,104 +59,15 @@ const VerifyEmail = (props) => {
             });
         }
     };
-
-    // const onSubmitPassword = async (values) => {
-    //     try {
-    //         setIsPasswordUpdateComplete(false);
-    //         const res = await updateCustomerApi({ customer_sid, req: { customer_account: updateCustomerPasswordRequest(values) } });
-    //         const customerDetail = res?.data;
-    //         setIsPasswordUpdateComplete(true);
-    //         setIsPasswordSuccessUpdated(true);
-    //         toast({
-    //             title: "Password updated",
-    //             description: "Please proceed with login!",
-    //             status: "success",
-    //             duration: 5000,
-    //             isClosable: true,
-    //           })
-    //     } catch(err) {
-    //         setIsPasswordSuccessUpdated(false);
-    //         setIsPasswordUpdateComplete(true);
-    //         toast({
-    //             title: "Error while updating password",
-    //             description: "Please retry or reach out to us.",
-    //             status: "error",
-    //             duration: 5000,
-    //             isClosable: true,
-    //         });
-    //     }
-    // };
+    
 
     useEffect(() => {
-        verifyToken();
-    }, [hash]);
+        if (hash)
+            verifyToken();
+    }, []);
 
     return (
         <CenterCard>
-            {/* <form>
-                <VStack>
-                    <RInput
-                        formProps = {getInputFormProps({
-                                id: "passcode",
-                                placeholder: "passcode",
-                                isRequired: true,
-                                minLength: 6,
-                            })
-                        }
-                        {
-                            ...form_props
-                        }
-                    />
-                    <RButton
-                        text="Verify Email"
-                        buttonProps={getButtonFormProps({
-                            isDisabled: !isValid || (isVerificationComplete && isVerificationSuccess),
-                            isLoading: isSubmitting,
-                        })}
-                    />
-                </VStack>
-            </form> */}
-            {/* {isVerificationComplete && isVerificationSuccess && (
-                <Box marginTop="10vh">
-                    <form onSubmit={handlePasswordSubmit(onSubmitPassword)}>
-                        <VStack>
-                            <RInput
-                                    formProps = {getInputFormProps({
-                                            id: "password",
-                                            placeholder: "password",
-                                            isRequired: true,
-                                            minLength: 6,
-                                            pattern: /[a-zA-Z0-9]+$/
-                                        })
-                                    }
-                                    register={passwordRegister}
-                                    errors={passwordErrors}
-                                    getFieldState={passwordGetFieldState}
-                                    size='lg'
-                            />
-                            <RButton
-                                text="Update Password"
-                                buttonProps={getButtonFormProps({
-                                    isDisabled: !isValidPassword,
-                                    isLoading: isSubmittingPassword,
-                                })}
-                            />
-                            <Box>
-                                {isPasswordUpdateComplete && isPasswordUpdatedSuccess &&  (
-                                <NavLink
-                                    to={CUSTOMER_LOGIN_URL()} 
-                                    style={{
-                                        color: "blue",
-                                        textDecoration: "underline"
-                                    }}
-                                >
-                                    proceed to login
-                                </NavLink>)}
-                            </Box>
-                        </VStack>
-                    </form>
-                </Box>
-            )} */}
             {isVerificationComplete && isVerificationSuccess &&  (
                 <Alert status='success'>
                     <AlertIcon />
